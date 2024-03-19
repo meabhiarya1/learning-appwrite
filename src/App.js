67c90610038d8190b0690bbe08fe40071ebb3c1f
Abhishek kumar <abhi.arya97161@gmail.com>
2024-03-19 12:21:31 +0530

    signup page created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />.
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
 }
 
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      login
-    </div>
-  )
-}
+    <>
+      
+    </>
+  );
+};
 
-export default Login
+export default Login;
