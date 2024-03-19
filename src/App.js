3a8cf4eaa8a85d287e5102d85202dd49dd33d8d2
Abhishek kumar <abhi.arya97161@gmail.com>
2024-03-19 11:40:30 +0530

    some files deleted and cleaned

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
