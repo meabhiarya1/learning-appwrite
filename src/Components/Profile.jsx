b47ef902aa515a840f5ea1eb2481f30198c04fa9
Abhishek kumar <abhi.arya97161@gmail.com>
2024-03-19 14:57:40 +0530

    profile added with logout functionality

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { account } from "../appwrite/appwriteConfig";
+import { useNavigate, NavLink } from "react-router-dom";
+import TodoForm from "./TodoForm";
+import Todos from "./Todos";
 
 const Profile = () => {
-  return (
-    <div>
-      Profile
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Profile
+  const [userDetails, setUserDetails] = useState();
+
+  useEffect(() => {
+    const getData = account.get();
+    getData.then(
+      function (response) {
+        console.log(response);
+        setUserDetails(response);
+      },
+
+      function (error) {
+        console.log(error);
+      }
+    );
+  }, []);
+
+  const logoutHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await account.deleteSession("current");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      {userDetails ? (
+        <>
+          <div className="min-h-min max-w-7xl mx-auto shadow-md flex justify-between text-right py-3 px-3 mt-2 rounded-md">
+            <div>
+              <p className="text-xl">Hello {userDetails.name.split(" ")[0]}</p>
+            </div>
+            <div>
+              <button
+                className="bg-red-400 text-white p-1 rounded-md"
+                onClick={logoutHandler}
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+          {/* TODO FORM */}
+          <TodoForm />
+          {/* TODOS BOX */}
+          <Todos />
+        </>
+      ) : (
+        <p className="mt-4">
+          Please Login To see Profile{" "}
+          <NavLink to="/">
+            <span className="bg-blue-300 p-2 cursor-pointer text-white rounded-md">
+              Login
+            </span>
+          </NavLink>
+        </p>
+      )}
+    </>
+  );
+};
+
+export default Profile;
